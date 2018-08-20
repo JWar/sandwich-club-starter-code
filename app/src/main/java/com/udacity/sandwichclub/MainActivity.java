113f582598c535bf9f2f874c321cbe146d9d593a
JonWar <jr5382@gmail.com>
2018-08-20 16:12:33 +0100

    Made changes to how NetworkUtils works: posts via parameters rather than body

--- app/src/main/java/com/udacity/sandwichclub/MainActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/MainActivity.java
@@ -7,10 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import com.udacity.sandwichclub.model.Contact;
 import com.udacity.sandwichclub.utils.NetworkUtils;
-
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,56 +20,81 @@ public class MainActivity extends AppCompatActivity {
 
     private NetworkAsyncTask networkAsyncTask;
 
+    /**
+     * onCreate is what is called when the App is run and MainActivity is started.
+     * I have done a few things in it
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Dont worry about super for now.
         super.onCreate(savedInstanceState);
+        //Tells the Activity what view/screen design it should use
         setContentView(R.layout.activity_main);
 
+        //This is the bit where you connect the views to Activity.
+        //Its so we can get the text entered in the form fields,
+        //and put them into the JSONObject (this is in onSave of course!)
         nameEditText = findViewById(R.id.main_name_edit_text);
         emailEditText = findViewById(R.id.main_email_edit_text);
         numberEditText = findViewById(R.id.main_number_edit_text);
-
     }
 
+    //This is what is called when the Save button is pressed.
     public void onSave(View aView) {
+        //Try and catch (at the bottom of this method) are just to pick up errors that may occur.
+        //Dont worry for now.
         try {
-            Toast.makeText(this, "Save pressed sending to server", Toast.LENGTH_SHORT).show();
+            //Get the text written in the forms then convert it to string.
             String nameString = nameEditText.getText().toString();
             String emailString = emailEditText.getText().toString();
             String numberString = numberEditText.getText().toString();
 
+            //Turn the text into a JSONObject with the correct keys
             JSONObject contactAsJSONObject = new JSONObject();
             contactAsJSONObject.put("name", nameString);
             contactAsJSONObject.put("email", emailString);
             contactAsJSONObject.put("number", numberString);
 
+            //Instantiate a new NetworkAsyncTask object. It does the Asynchronous work,
+            //and makes the network calls to the servlet.
             networkAsyncTask = new NetworkAsyncTask();
 
+            //Execute is what tells the NetworkAsyncTask to do the network work. Does this
+            //using the JSONObject we created above, which we want POSTing to the servlet.
             networkAsyncTask.execute(contactAsJSONObject);
         } catch (Exception e) {
             Log.i("starter","Error in MainActivity.onSave: "+e.getLocalizedMessage());
         }
-
     }
-
-    public void onCancel(View aView) {
-        Toast.makeText(this, "Cancel pressed clearing fields", Toast.LENGTH_SHORT).show();
-        nameEditText.setText(null);
-        emailEditText.setText(null);
-        numberEditText.setText(null);
-    }
-
+    //This is my custom AsyncTask.
     public class NetworkAsyncTask extends AsyncTask<JSONObject,Void,String> {
         @Override
         protected String doInBackground(JSONObject... aJSONObjects) {
+            //This is now a background thread.
+            //Get the JSONObject from the parameters in doInBackground.
+            //Notice the parameter in doInBackground looks a little different than
+            //the other parameters.
+            //From this can you guess why we need to use [0]? What does this remind you of?
             JSONObject objectToSend = aJSONObjects[0];
+
+            //This is then the bit where the networking is called.
+            //Control + Click on .sendContactDetails to take you to that method
             String response = NetworkUtils.sendContactDetails(objectToSend);
+
+            //Response received, now pass it to onPostExecute below.
+            //We are still on the background thread now. Up until the return is
+            //finished.
             return response;
         }
 
         @Override
         protected void onPostExecute(String aResponse) {
-            if (aResponse!=null) {
+            //This is where the response ends up. It is now up to you what you do
+            //with it.
+            //Note you are now on the Main Thread again!
+            if (aResponse!=null) {//This is just to check there were no errors, vital to handle them properly
+                //A Toast is a little message that pops up at the bottom of your screen.
+                //Im sure youll have seen one before in Android apps.
                 Toast.makeText(MainActivity.this,
                         "Response received:" +
                                 aResponse, Toast.LENGTH_SHORT).show();
@@ -81,5 +103,11 @@ public class MainActivity extends AppCompatActivity {
                         "Problem with network response", Toast.LENGTH_SHORT).show();
             }
         }
+    }
+    //This is what is called when the Cancel button is pressed
+    public void onCancel(View aView) {
+        nameEditText.setText(null);
+        emailEditText.setText(null);
+        numberEditText.setText(null);
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/NetworkUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/NetworkUtils.java
@@ -9,6 +9,7 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Iterator;
 import java.util.Scanner;
 import static com.udacity.sandwichclub.MainActivity.LOG_TAG;
 
@@ -18,17 +19,129 @@ import static com.udacity.sandwichclub.MainActivity.LOG_TAG;
  */
 public class NetworkUtils {
 
-    private static final String URL_STRING = "http://10.0.2.2:8080/jrclient/contact.aj";
+    private static final String URL_STRING = "http://10.0.2.2:8080/jrclient_1.0.2/storetxn.aj";
 
+    //This kicks off the Networking process. It takes the JSONObject parameter and calls
+    //the other methods in this class, which make sure the JSONObject is bundled up
+    //in the correct format and POSTed properly.
+    //It returns the results for whatever called the method originally. In our case,
+    //the NetworkAsyncTask in MainActivity.
     public static synchronized String sendContactDetails(JSONObject aJSONObjectToSend) {
         try {
             String serverResponseReceived = postJSONObject(aJSONObjectToSend);
             return serverResponseReceived;
         } catch (Exception e) {
-            Log.i(LOG_TAG,"Error in NetworkUtils.getMoviesOrderBy: "+e.getLocalizedMessage());
+            Log.i(LOG_TAG,"Error in NetworkUtils.sendContactDetails: "+e.getLocalizedMessage());
             return null;
         }
     }
+    private static String postJSONObject(JSONObject aJSONObjectToSend) throws Exception {
+        try {
+            //Dont think I need to say what this is... Dont worry too much about what buildUrl is doing.
+            //Android likes to complicate things... for good reasons mind you.
+            URL url = buildUrl();
+
+            //This is the object that handle the network connections.
+            //You use these objects methods to get/post data
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            connection.setConnectTimeout(5000);
+            connection.setRequestMethod("POST");
+
+            //This is the posting bit!
+            DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+            //This is the string we are posting. It is the JSONObject in a particular
+            //string format, which aligns with what the servlet expects.
+            String requestToSend = getQuery(aJSONObjectToSend);
+            wr.writeBytes(requestToSend);
+
+            //House keeping. Making sure it is finished properly
+            wr.flush();
+            //House keeping. Making sure it is finished properly
+            wr.close();
+
+            //This is the response we have received from the servlet after the post.
+            InputStream in = connection.getInputStream();
+
+            //Dont worry about this. It just makes sure the response is in the correct format.
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            //The hasNext() and next() methods are similar to the iterator methods in
+            //the getQuery method. Basically it asks if there is something to get, then
+            //gets it! Anything 'got' is put into the response string.
+            boolean hasInput = scanner.hasNext();
+            String response = null;
+            if (hasInput) {
+                response = scanner.next();
+            }
+            //House keeping. Making sure it is finished properly
+            scanner.close();
+            //Lets the Log know what we have got.
+            Log.i(LOG_TAG, "HttpUrl response: " + response);
+            //This return is what ends up in the NetworkAsyncTask.onPostExecute in MainActivity
+            //via sendContactDetails() above!
+            return response;
+        } catch (Exception e) {
+            Log.i(LOG_TAG, "Error in NetworkUtils.postJSONObject: "+e.getLocalizedMessage());
+            return null;
+        }
+    }
+
+    /**
+     * This converts a JSON objects keys/values into a servlet friendly string
+     * for the servlet to parse properly.
+     * The format goes key1=value1&key2=value2&...
+     */
+    private static String getQuery(JSONObject aJSONObject) throws Exception {
+        //This is the string we will return, we will build it up using the key/value in the JSONObject
+        String result = "";
+        //Ensures the & sign isnt put in in the wrong place.
+        boolean first = true;
+        //This is part of the JSONObject functionality and allows you to
+        //loop over the JSONObject and treat it as a list.
+        //This is the .keys() method which returns the iterator
+        Iterator<String> iterator = aJSONObject.keys();
+        while (iterator.hasNext()) {//Check to see if the iterator has a next element in its list.
+            //Get the key from the next element in the iterator.
+            String key = iterator.next();
+            //Get the value using the key from the JSONObject
+            String value = aJSONObject.getString(key);
+            //This if/else ensures the & doesnt appear in the wrong place (i.e. key=value&)
+            if (first) {
+                first = false;
+            } else {
+                result += "&";
+            }
+            //This is where the string is built up using the key and value with an '=' in between
+            result += key;
+            result += "=";
+            result += value;
+        }
+        //After the loop has finished return the string!
+        return result;
+    }
+    /**
+     * URLs arent just simple strings. Android needs to do a couple of things to them to
+     * make sure theyre in the correct format. This is what this method does. Dont worry too much about
+     * it.
+     */
+    private static URL buildUrl() {
+        String urlToParse = URL_STRING;
+        Uri uri = Uri.parse(urlToParse)
+                .buildUpon()
+                .build();
+        try {
+            URL url = new URL(uri.toString());
+            Log.i(LOG_TAG, "URL: " + url);
+            return url;
+        } catch (MalformedURLException e) {
+            Log.i(LOG_TAG, "Error in BuildURL: " + e.getLocalizedMessage());
+            return null;
+        }
+    }
+    //Not used yet, but Im sure you can guess what this does?
     private static String getResponseFromHttpUrl(URL url) throws IOException {
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
         try {
@@ -47,53 +160,6 @@ public class NetworkUtils {
             return response;
         } finally {
             urlConnection.disconnect();
-        }
-    }
-    private static String postJSONObject(JSONObject aJSONObjectToSend) throws Exception {
-        URL url = buildUrl();
-
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setDoOutput(true);
-        connection.setDoInput(true);
-        connection.setInstanceFollowRedirects(false);
-        connection.setRequestMethod("POST");
-        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-        connection.setRequestProperty("charset", "utf-8");
-
-        DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
-
-        String stringToSend = aJSONObjectToSend.toString();
-        wr.writeBytes(stringToSend);
-
-        wr.flush();
-        wr.close();
-
-        InputStream in = connection.getInputStream();
-
-        Scanner scanner = new Scanner(in);
-        scanner.useDelimiter("\\A");
-
-        boolean hasInput = scanner.hasNext();
-        String response = null;
-        if (hasInput) {
-            response = scanner.next();
-        }
-        scanner.close();
-        Log.i(LOG_TAG, "HttpUrl response: " + response);
-        return response;
-    }
-    private static URL buildUrl() {
-        String urlToParse = URL_STRING;
-        Uri uri = Uri.parse(urlToParse)
-                .buildUpon()
-                .build();
-        try {
-            URL url = new URL(uri.toString());
-            Log.i(LOG_TAG, "URL: " + url);
-            return url;
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-            return null;
         }
     }
 }
